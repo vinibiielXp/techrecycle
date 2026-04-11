40acfccb9174b2250f264835f06c29a1116bdf16
Your Name <you@example.com>
2026-04-11 02:40:22 -0300

    config banco railway

--- config/db.js
+++ config/db.js
@@ -1,21 +1,24 @@
 const mysql = require("mysql2");
 
-const conn = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
+// Testar conexão
 pool.getConnection((err, conn) => {
   if (err) {
-    console.error('Erro ao conectar ao MySQL:', err);
+    console.error("Erro ao conectar no banco:", err);
   } else {
-    console.log('Conectado ao MySQL !');
+    console.log("Conectado ao MySQL!");
     conn.release();
   }
 });
 
-module.exports = conn;
+module.exports = pool;
